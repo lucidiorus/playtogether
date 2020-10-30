d50074effcd1285f8ec5f0c3100f5a8ddebf817a
Lucidio <lucidiovacas@gmail.com>
2020-10-30 18:44:05 +0100

    Initial commit

---
+++ src/test/java/com/lucidiovacas/playtogether/PlaytogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucidiovacas.playtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaytogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
